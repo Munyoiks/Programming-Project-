2052764188a6791e687c8a692f34cd64925e9cbf
Munyoiks <167209005+Munyoiks@users.noreply.github.com>
2024-07-08 11:24:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,7 +41,51 @@ void addStudent() {
     std::cin >> student.group;
 
 
-int main() {
+int choice;
+        int choice;
+    do {
+        std::cout << "Choose activity type: 1. Sport 2. Club/Society 3. Done: ";
+        std::cin >> choice;
+
+        if (choice == 1) {
+            std::cout << "Choose sport: ";
+            for (size_t i = 0; i < sports.size(); ++i) {
+                std::cout << i+1 << ". " << sports[i].name << " (" << sports[i].members.size() << "/" << sports[i].max_capacity << ")\n";
+            }
+            int sport_choice;
+            std::cin >> sport_choice;
+            if (sport_choice >= 1 && sport_choice <= sports.size()) {
+                if (sports[sport_choice-1].members.size() < sports[sport_choice-1].max_capacity) {
+                    student.activities.push_back(sports[sport_choice-1].name);
+                    sports[sport_choice-1].members.push_back(student);
+                } else {
+                    std::cout << "Sport is at full capacity.\n";
+                }
+            }
+        } else if (choice == 2) {
+            std::cout << "Choose club/society: ";
+            for (size_t i = 0; i < clubs.size(); ++i) {
+                std::cout << i+1 << ". " << clubs[i].name << " (" << clubs[i].members.size() << "/" << clubs[i].max_capacity << ")\n";
+            }
+            int club_choice;
+            std::cin >> club_choice;
+            if (club_choice >= 1 && club_choice <= clubs.size()) {
+                if (clubs[club_choice-1].members.size() < clubs[club_choice-1].max_capacity) {
+                    student.activities.push_back(clubs[club_choice-1].name);
+                    clubs[club_choice-1].members.push_back(student);
+                } else {
+                    std::cout << "Club/Society is at full capacity.\n";
+                }
+            }
+        }
+    } while (choice != 3);
+
+    students.push_back(student);
+}
+
+void viewStudents() {
+    for (const auto& student : students) {
+
     
     return 0;
 }
